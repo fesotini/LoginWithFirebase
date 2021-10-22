c65600a301055a30d653813fabe7b38f8bc900ab
fesotini <mfsz2468@gmail.com>
2021-10-22 09:34:37 -0500

    Integration of Firebase's libraries

--- app/src/main/java/com/example/loginwithfirebase/MainActivity.kt
+++ app/src/main/java/com/example/loginwithfirebase/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.loginwithfirebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.analytics.FirebaseAnalytics
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        /*
+        Thread.sleep(2000)
+        setTheme(R.style.AppTheme)
+        */
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //
+        val analytics = FirebaseAnalytics.getInstance(this)
+        val bundle = Bundle()
+        bundle.putString("message", "Integración de Firebase Completa")
+        analytics.logEvent("InitScreen", bundle)
     }
 }
